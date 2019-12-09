8c0835868fa5a1ea7cc76b17ac11a0d8a8ddd65e
Dave Kalu <davekalu46@gmail.com>
2019-12-09 08:40:34 +0100

    Updates

---
+++ precache-manifest.0c59a6c0f4d4b0d69bb6f75d67d74c54.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b230d7358d03136d0b17f3a9481d91f",
+    "url": "/exchange-rate-calculator-widget/index.html"
+  },
+  {
+    "revision": "201aeaade7eb4ce278c6",
+    "url": "/exchange-rate-calculator-widget/static/css/2.6d0a65c4.chunk.css"
+  },
+  {
+    "revision": "40540d67809d8da0d0f5",
+    "url": "/exchange-rate-calculator-widget/static/css/main.bd0493ab.chunk.css"
+  },
+  {
+    "revision": "201aeaade7eb4ce278c6",
+    "url": "/exchange-rate-calculator-widget/static/js/2.3a607d22.chunk.js"
+  },
+  {
+    "revision": "40540d67809d8da0d0f5",
+    "url": "/exchange-rate-calculator-widget/static/js/main.3d94ba80.chunk.js"
+  },
+  {
+    "revision": "0f052a637550de33e4ea",
+    "url": "/exchange-rate-calculator-widget/static/js/runtime-main.715f6ae6.js"
+  },
+  {
+    "revision": "9be71f7bbfa5e5c8eb445022e83315b5",
+    "url": "/exchange-rate-calculator-widget/static/media/rep.9be71f7b.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/exchange-rate-calculator-widget/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/exchange-rate-calculator-widget/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/exchange-rate-calculator-widget/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/exchange-rate-calculator-widget/static/media/slick.f97e3bbf.svg"
+  }
+]);
